1f835903ce4eb098d56f652e882a4e5fdf7daeef
phenomenal <brijeshshah654@gmail.com>
2024-02-03 19:02:48 +0530

    Completed till login form

--- script.js
+++ script.js
@@ -1,8 +1,60 @@
-document.getElementById('next').onclick = function () {
-    let lists = document.querySelectorAll('.item');
-    document.getElementById('slide').appendChild(lists[0]);
-}
-document.getElementById('prev').onclick = function () {
-    let lists = document.querySelectorAll('.item');
-    document.getElementById('slide').prepend(lists[lists.length - 1]);
-}
+// Capthcha
+(function () {
+    const fonts = ['cursive', 'sans', 'serif', 'monospace', 'sans-serif']
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let captchaval = "";
+
+
+    // Captcha generation
+    function generateString() {
+        length = 5
+        let result = '';
+        const charactersLength = characters.length;
+        for (let i = 0; i < length; i++) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        captchaval = result;
+    }
+
+
+    // captcha placing at preview
+    function setcaptcha() {
+        let html = captchaval.split("").map((char) => {
+            const rotate = -20 + Math.trunc(Math.random() * 30)
+            const font = Math.trunc(Math.random() * fonts.length)
+            return `<span 
+            style="
+            transform:rotate(${rotate}deg);
+            font-family:${fonts[font]}
+            "
+            >${char}</span>`;
+        }).join("");
+        document.querySelector(".preview").innerHTML = html
+    }
+
+
+    // captcha initialization
+    function initcaptcha() {
+        generateString();
+        setcaptcha()
+    }
+    initcaptcha()
+
+
+    // Varifying both captcha
+    document.querySelector(".btn").addEventListener("click", function () {
+        let inputcaptchaval = document.querySelector(".captcha input").value;
+        if (inputcaptchaval == captchaval) {
+            alert("Logging In!")
+        } else {
+            alert("Invalid captcha!")
+        }
+    })
+})();
+
+
+// refresh captcha button
+document.querySelector(".captcha-refresh").addEventListener("click", function () {
+    generateString();
+    setcaptcha()
+})
